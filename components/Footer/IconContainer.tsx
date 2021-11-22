6929ecb909a759f07bd6ee57aaea0ac3d8311744
Pavel Zapletal <pavel.zaplet25@gmail.com>
2021-11-22 12:18:25 +0100

    mobile layout

--- components/Footer/IconContainer.tsx
+++ components/Footer/IconContainer.tsx
@@ -25,15 +25,16 @@ const StyledIconContainer = styled.a`
 	display: inline-flex;
 	position: relative;
 	padding: 0.65rem;
-	border-radius: 50%;
+	border-radius: 0.5rem;
 	transition: all 0.3s;
+	border: 1px solid var(--col1);
 
 	& + & {
 		margin-left: 1rem;
 	}
 
 	&:hover {
-		background-color: var(--second-col);
+		border: 1px solid var(--bg-col);
 		transition: all 0.3s;
 	}
 `;

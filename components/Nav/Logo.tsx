c02bdfaf051ee10e32b4eac012ecafa8124506f7
Pavel Zapletal <pavel.zaplet25@gmail.com>
2021-11-22 17:19:44 +0100

    mobile layout

--- components/Nav/Logo.tsx
+++ components/Nav/Logo.tsx
@@ -3,14 +3,28 @@ import styled from "styled-components";
 import { animateScroll as scroll } from "react-scroll";
 
 const Logo = () => {
-	return <StyledLogo onClick={() => scroll.scrollToTop()}>PZ</StyledLogo>;
+	return (
+		<StyledLogo onClick={() => scroll.scrollToTop()}>portfolio</StyledLogo>
+	);
 };
 
 export default Logo;
 
 const StyledLogo = styled.div`
-	font-size: var(--fosi-nav);
+	position: relative;
+	font-size: 2.5rem;
+	font-weight: var(--fowe-bold);
 	margin-left: var(--horizontal-gap);
 	z-index: 10;
 	cursor: pointer;
+
+	&::before {
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0.15rem;
+		width: 1rem;
+		height: 0.25rem;
+		background-color: var(--col1);
+	}
 `;

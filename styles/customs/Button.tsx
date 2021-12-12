76e577a420018200ef7a0742e8fedc3c9ddb4c82
Pavel Zapletal <pavel.zaplet25@gmail.com>
2021-12-12 21:47:28 +0100

    dd

--- styles/customs/Button.tsx
+++ styles/customs/Button.tsx
@@ -42,29 +42,32 @@ export const Button = ({
 const ButtonGlobal = styled.button`
 	border: none;
 	font-family: inherit;
-	box-shadow: var(--shadow);
 	cursor: pointer;
+	border-radius: 0.6rem;
 
-	p,
 	a {
 		display: inline-flex;
-		align-items: center;
-		padding: 0.4rem 0.8rem;
+		gap: 1rem;
+	}
+
+	p {
 		font-size: var(--f-btn);
+	}
+
+	a {
+		padding: 0.8rem 0.9rem;
 	}
 
 	&:hover {
 		transition: transform 0.3s linear;
-		transform: scale(1.1);
+		transform: scale(1.06);
 	}
 `;
 
 const ButtonSecondary = styled(ButtonGlobal)`
-	border-radius: 0.6rem;
-	background: var(--bg-col);
-	border: 1px solid var(--col1);
+	box-shadow: 0 0 20px rgba(8, 1, 1, 0.125);
+	/* border: 1px solid var(--col1); */
 
-	p,
 	a {
 		margin: 0 0.5rem;
 	}
@@ -73,15 +76,18 @@ const ButtonSecondary = styled(ButtonGlobal)`
 const ButtonPrimary = styled(ButtonGlobal)`
 	border: 1px solid var(--col1);
 	background-color: var(--col1);
-	border-radius: 0.6rem;
+	box-shadow: var(--shadow);
 
 	span {
 		display: inline-block;
-		margin: 0 0.25rem !important;
+		/* margin: 0 0.25rem !important; */
 	}
-	p,
-	a {
+
+	p {
 		color: var(--bg-col);
-		margin-right: 0.25rem;
+	}
+
+	a {
+		margin: 0 0.6rem;
 	}
 `;

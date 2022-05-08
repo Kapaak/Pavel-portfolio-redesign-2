cbd7233e1ac215e8e0a485d969ac89b5f819164c
kapaak <pavel.zaplet25@gmail.com>
2022-05-08 23:22:07 +0200

    meta tags included

--- styles/customs/Head.tsx
+++ styles/customs/Head.tsx
@@ -10,6 +10,15 @@ const HeadSelector = () => {
 				href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&display=swap"
 				rel="stylesheet"
 			/>
+			<meta name="author" content="Pavel Zapletal" />
+			<meta
+				name="keywords"
+				content="Pavel Zapletal, React.js, Next.js, web portfolio"
+			/>
+			<meta
+				name="description"
+				content="Hi, my name is Pavel Zapletal and this is my portfolio website. I am a website creator, designer and programmer :)."
+			/>
 		</Head>
 	);
 };

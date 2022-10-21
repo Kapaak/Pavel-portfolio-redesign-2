5a424223958840ce9d93c911dc941ef97106817f
Pavel Zapletal <pavel.zaplet25@gmail.com>
2022-10-21 19:29:26 +0200

    feat: update url

--- data.tsx
+++ data.tsx
@@ -9,81 +9,81 @@ import ChatAppImage from "./public/images/projects/chat-app.png";
 import Routist from "./public/images/projects/routist.png";
 
 export const projectData: Array<ProjectType> = [
-	{
-		title: "Routist",
-		colors: ["#313873", "#1462ad", "#f9f9f9"],
-		url: "https://routist.vercel.app/",
-		image: Routist,
-		fonts: ["Titillium Web"],
-		tech: ["Next.js", "Next Auth", "Prisma"],
-	},
-	{
-		title: "Kometa Lužánky",
-		colors: ["#2172DD", "#FFCA0F", "#FEFEFE"],
-		url: "https://kometa-plavani.vercel.app/",
-		image: Kometa,
-		fonts: ["Bebas Neue", "Poppins"],
-		tech: ["Next.js", "Sanity.io"],
-	},
-	{
-		title: "Kuchta Buchta",
-		colors: ["#B0235F", "#FA7268", "#363636"],
-		url: "https://kuchta-buchta.vercel.app/",
-		image: KuchtaBuchtaImage,
-		fonts: ["Oswald", "Rajdhani"],
-		tech: ["Next.js", "Sanity.io"],
-	},
-	{
-		title: "Barbora Nováková",
-		colors: ["#F8F8F8", "#8D0801", "#001427 "],
-		url: "https://barboranovakova.cz/",
-		image: BarboraNovakovaImage,
-		fonts: ["Roboto", "Righteous"],
-		tech: ["Next.js", "Sanity.io"],
-	},
+  {
+    title: "Routist",
+    colors: ["#313873", "#1462ad", "#f9f9f9"],
+    url: "https://routist.vercel.app/",
+    image: Routist,
+    fonts: ["Titillium Web"],
+    tech: ["Next.js", "Next Auth", "Prisma"],
+  },
+  {
+    title: "Plavání Lužánky",
+    colors: ["#2172DD", "#FFCA0F", "#FEFEFE"],
+    url: "https://plavani-luzanky.cz/",
+    image: Kometa,
+    fonts: ["Bebas Neue", "Poppins"],
+    tech: ["Next.js", "Sanity.io"],
+  },
+  {
+    title: "Kuchta Buchta",
+    colors: ["#B0235F", "#FA7268", "#363636"],
+    url: "https://kuchta-buchta.vercel.app/",
+    image: KuchtaBuchtaImage,
+    fonts: ["Oswald", "Rajdhani"],
+    tech: ["Next.js", "Sanity.io"],
+  },
+  {
+    title: "Barbora Nováková",
+    colors: ["#F8F8F8", "#8D0801", "#001427 "],
+    url: "https://barboranovakova.cz/",
+    image: BarboraNovakovaImage,
+    fonts: ["Roboto", "Righteous"],
+    tech: ["Next.js", "Sanity.io"],
+  },
 
-	{
-		title: "Rick and Morty",
-		colors: ["#001427", "#A2F3BC", "#F5BB40"],
-		url: "https://rick-a-morty.vercel.app/",
-		image: RickAndMortyImage,
-		fonts: ["Rajdhani"],
-		tech: ["Next.js", "Rick and Morty API"],
-	},
-	{
-		title: "Chat app",
-		colors: ["#ececec", "#163cff"],
-		url: "https://chat-app-final.vercel.app/",
-		image: ChatAppImage,
-		fonts: ["Raleway"],
-		tech: ["Next.js", "Pusher"],
-	},
+  {
+    title: "Rick and Morty",
+    colors: ["#001427", "#A2F3BC", "#F5BB40"],
+    url: "https://rick-a-morty.vercel.app/",
+    image: RickAndMortyImage,
+    fonts: ["Rajdhani"],
+    tech: ["Next.js", "Rick and Morty API"],
+  },
+  {
+    title: "Chat app",
+    colors: ["#ececec", "#163cff"],
+    url: "https://chat-app-final.vercel.app/",
+    image: ChatAppImage,
+    fonts: ["Raleway"],
+    tech: ["Next.js", "Pusher"],
+  },
 ];
 
 export const footerData: Array<FooterType> = [
-	{
-		icon: "/icons/codepen.svg",
-		link: "https://codepen.io/Kapaak",
-		name: "codepen icon",
-	},
-	{
-		icon: "/icons/github.svg",
-		link: "https://github.com/Kapaak",
-		name: "github icon",
-	},
-	{
-		icon: "/icons/linkedin.svg",
-		link: "https://www.linkedin.com/in/pavel-zapletal-903bb2188/",
-		name: "linkedin icon",
-	},
-	{
-		icon: "/icons/instagram.svg",
-		link: "https://www.instagram.com/pavel_kapaak/",
-		name: "instagram icon",
-	},
-	{
-		icon: "/icons/facebook.svg",
-		link: "https://www.facebook.com/pavel.kappa.zapletal",
-		name: "facebook icon",
-	},
+  {
+    icon: "/icons/codepen.svg",
+    link: "https://codepen.io/Kapaak",
+    name: "codepen icon",
+  },
+  {
+    icon: "/icons/github.svg",
+    link: "https://github.com/Kapaak",
+    name: "github icon",
+  },
+  {
+    icon: "/icons/linkedin.svg",
+    link: "https://www.linkedin.com/in/pavel-zapletal-903bb2188/",
+    name: "linkedin icon",
+  },
+  {
+    icon: "/icons/instagram.svg",
+    link: "https://www.instagram.com/pavel_kapaak/",
+    name: "instagram icon",
+  },
+  {
+    icon: "/icons/facebook.svg",
+    link: "https://www.facebook.com/pavel.kappa.zapletal",
+    name: "facebook icon",
+  },
 ];
